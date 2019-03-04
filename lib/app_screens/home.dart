aa3bf3f234fc6422a2e6e8f3398fdf841d64f466
Rudrik Patel <rudrik.patel@zymr.com>
2019-03-04 12:21:59 +0530

    ImageAsset -- Implemented the Button and the Alert popup dialog.

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -94,7 +94,8 @@ class Home extends StatelessWidget {
                     )
                   ],
                 ),
-                FlightImageAsset()
+                FlightImageAsset(),
+                FlightBookButton()
               ],
             )));
   }
@@ -106,5 +107,37 @@ class FlightImageAsset extends StatelessWidget {
     AssetImage assetImage = AssetImage('images/ic_dislike.png');
     Image image = Image(image: assetImage, width: 250.0, height: 250.0);
     return Container(child: image);
+  }
+}
+
+class FlightBookButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 250.0,
+      height: 50.0,
+      child: RaisedButton(
+          color: Colors.deepOrange,
+          child: Text(
+            "Book Your Flight",
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w700),
+          ),
+          elevation: 6.0,
+          onPressed: () => bookFlight(context)),
+    );
+  }
+
+  void bookFlight(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("Flight book successfully"),
+      content: Text("Have a pleasant flight"),
+    );
+    showDialog(
+        context: context, builder: (BuildContext context) => alertDialog);
   }
 }
