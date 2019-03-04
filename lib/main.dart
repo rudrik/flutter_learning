59fa37aa36d7dfb96a43e8f8d4a5d84a98653379
Rudrik Patel <rudrik.patel@zymr.com>
2019-03-04 12:47:02 +0530

    Develop -- Merged with develop

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 import './app_screens/home.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Exploring UI widgets",
-    home: Home(),
-  ));
+void main() => runApp(new MyFlutterApp());
+
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "My Materaial App",
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("My First app screen"),
+            ),
+            body: Home()));
+  }
 }
