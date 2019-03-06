9db05f326b452a5dcce6ea75c61fff64239e7638
Rudrik Patel <rudrik.patel@zymr.com>
2019-03-06 15:37:26 +0530

    InterestCalculus -- Implemented the design of the Interest Calculus.

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,132 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyFlutterApp());
+void main() {
+  runApp(MaterialApp(
+    title: "Simple Interest calculator App",
+    home: SIForm(),
+  ));
+}
 
-class MyFlutterApp extends StatelessWidget {
+class SIForm extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "My Materaial App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My First app screen"),
-        ),
-        body: getLongListView(),
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            tooltip: "Add One More Item",
-            onPressed: () {
-              debugPrint("FAB Clicked");
-            }),
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return _SIFormState();
   }
 }
 
-void showSnackBar(BuildContext context, String listItem) {
-  var snackBar = SnackBar(
-    content: Text("You just tapped $listItem"),
-    duration: Duration(milliseconds: 500),
-    action: SnackBarAction(
-        label: "UNDO",
-        onPressed: () {
-          debugPrint("Permfoming dummy undo.");
-        }),
-  );
+class _SIFormState extends State<SIForm> {
+  var _currencies = ['Rupees', 'Dollars', 'Pounds'];
+  var _minimumPadding = 5.0;
+  var _defaultCurrencies = 'Rupees';
 
-  Scaffold.of(context).showSnackBar(snackBar);
-}
-
-Widget getListView() {
-  var listView = ListView(
-    children: <Widget>[
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Nice View"),
-        trailing: Icon(Icons.wb_sunny),
-        onTap: () {
-          debugPrint("TAPPED!");
-        },
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text('Simple Interest Calculator'),
       ),
-      ListTile(
-        leading: Icon(Icons.android),
-        title: Text("Android"),
+      body: Container(
+        margin: EdgeInsets.all(_minimumPadding * 2),
+        child: ListView(
+          children: <Widget>[
+            getImageAsset(),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: 'Principal',
+                        hintText: 'Enter Principal e.g. 12000',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(_minimumPadding),
+                        )))),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Rate of Interest',
+                      hintText: 'In percent',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(_minimumPadding),
+                      ),
+                    ))),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: 'Term',
+                              hintText: 'Time in years',
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.circular(_minimumPadding),
+                              ),
+                            ))),
+                    Container(
+                      width: _minimumPadding * 5,
+                    ),
+                    Expanded(
+                        child: DropdownButton<String>(
+                      items: _currencies.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      value: _defaultCurrencies,
+                      onChanged: (String newValueSelected) {
+                        setState(() {});
+                      },
+                    ))
+                  ],
+                )),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                      padding: EdgeInsets.only(left: _minimumPadding * 5),
+                      child: RaisedButton(
+                          child: Text('Calculate'), onPressed: () {}),
+                    )),
+                    Expanded(
+                      child: Container(
+                          padding: EdgeInsets.only(left: _minimumPadding * 5),
+                          child: RaisedButton(
+                              child: Text('Reset'), onPressed: () {})),
+                    )
+                  ],
+                )),
+            Padding(
+              padding: EdgeInsets.all(_minimumPadding),
+              child: Text('Todo Text'),
+            )
+          ],
+        ),
       ),
-      ListTile(
-        leading: Icon(Icons.devices),
-        title: Text("Samsung"),
-      ),
-      Text("Another text element"),
-      Container(color: Colors.red, height: 30.0)
-    ],
-  );
-  return listView;
-}
+    );
+  }
 
-List<String> getListElements() {
-  var items = List<String>.generate(1000, (counter) => "Item $counter");
-  return items;
-}
-
-Widget getLongListView() {
-  var listItem = getListElements();
-  var listView = ListView.builder(itemBuilder: (context, index) {
-    if (index < listItem.length)
-      return ListTile(
-        leading: Icon(Icons.arrow_right),
-        title: Text(listItem[index]),
-        onTap: () {
-          showSnackBar(context, listItem[index]);
-        },
-      );
-  });
-
-  return listView;
+  Widget getImageAsset() {
+    AssetImage assetImage = AssetImage('images/ic_dislike.png');
+    Image image = Image(
+      image: assetImage,
+      width: 125.0,
+      height: 125.0,
+    );
+    return Container(
+      child: image,
+      margin: EdgeInsets.all(_minimumPadding * 10),
+    );
+  }
 }
