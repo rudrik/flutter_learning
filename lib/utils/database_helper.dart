85ace3a68bdb13b25a628775a5e6d315b0f4059c
Rudrik Patel <rudrik.patel@zymr.com>
2019-03-19 14:27:15 +0530

    NoteKeeper -- Commented the code properly.

--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -7,8 +7,8 @@ import 'package:sqflite/sqflite.dart';
 import '../models/note.dart';
 
 class DatabaseHelper {
-  static DatabaseHelper _databaseHelper; //Singleton DatabaseHelper
-  static Database _database;
+  static DatabaseHelper _databaseHelper; //singleton DatabaseHelper
+  static Database _database; //singleton database
 
   String noteTable = 'note_table';
   String colId = 'id';
@@ -17,11 +17,12 @@ class DatabaseHelper {
   String colPriority = 'priority';
   String colDate = 'date';
 
-  DatabaseHelper._createInstance();
+  DatabaseHelper._createInstance(); // Named constructor to create instance of databaseHelper
 
   factory DatabaseHelper() {
     if (_databaseHelper == null) {
-      _databaseHelper = DatabaseHelper._createInstance();
+      _databaseHelper = DatabaseHelper
+          ._createInstance(); // This is executed only once, Singleton object
     }
     return _databaseHelper;
   }
@@ -51,17 +52,15 @@ class DatabaseHelper {
   }
 
 //Fetch Operation: get all note objects from database
-
   Future<List<Map<String, dynamic>>> getNoteMapList() async {
     Database db = await this.database;
 
-//  var result= await db.rawQuery('SELECT * FROM $noteTable order by $colPriority ASC');
+    // var result= await db.rawQuery('SELECT * FROM $noteTable order by $colPriority ASC');
     var result = await db.query(noteTable, orderBy: '$colPriority ASC');
     return result;
   }
 
-//Insert operation:Insert a note object to database
-
+  //Insert operation:Insert a note object to database
   Future<int> insertNote(Note note) async {
     Database db = await this.database;
     var result = await db.insert(noteTable, note.toMap());
@@ -69,13 +68,13 @@ class DatabaseHelper {
   }
 
   // Update Operation: Update a note object ans save it to database
-
   Future<int> updateNote(Note note) async {
     var db = await this.database;
     var result = await db.update(noteTable, note.toMap(),
         where: '$colId = ?', whereArgs: [note.id]);
     return result;
   }
+
 // Delete Operation : delete a note object from the database.
   Future<int> deleteNote(int id) async {
     var db = await this.database;
@@ -83,7 +82,6 @@ class DatabaseHelper {
         await db.rawDelete('DETELE FROM $noteTable WHERE $colId = $id');
     return result;
   }
-
 
   // Get the count of total note from the database
   Future<int> getCount() async {
